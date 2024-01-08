0bff2e82ca06ab646e4c926d6abb97ff45ff5f04
= <alain.deblois@hotmail.com>
2024-01-07 19:06:57 -0500

    2nd commit Add ModalView

---
+++ GitTest/ModalView.swift
@@ -0,0 +1,18 @@
+//
+//  ModalView.swift
+//  GitTest
+//
+//  Created by Alain Deblois on 2024-01-07.
+//
+
+import SwiftUI
+
+struct ModalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ModalView()
+}
